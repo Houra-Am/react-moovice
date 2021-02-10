cd5cde1e9894f151a0acc92ad4e5e7460d153193
Nicolas Gallego <gallagan@MacBook-Pro-de-Nicolas.local>
2021-02-10 15:13:54 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
