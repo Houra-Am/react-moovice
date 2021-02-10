dcc3b1787565cc5314c1a2fcde50c5154a714a70
Houra-Am <61083815+Houra-Am@users.noreply.github.com>
2021-02-10 16:56:40 +0100

    gitignore added

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,11 +1,34 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class Popular extends Component {
-    render() {
-        return (
-            <div>
-                <h1>Popular</h1>
-            </div>
-        )
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      movies: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch(
+      "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=c72ac9cf5197a1f66ad0d776f5568bc2"
+    )
+      .then((response) => response.json())
+      .then((popular) => {
+        console.log("popular", popular);
+        this.setState({
+          movies: popular.results,
+        });
+      })
+      .catch((error) => console.error(error));
+  }
+
+  render() {
+    console.log("movie=", this.state.movies);
+
+    return (
+      <div>
+        <h1>Popular</h1>
+      </div>
+    );
+  }
 }
